084d2583929954e014944cf30126aa351547194d
Jhonatan Quihuiri <jumpinjhonatan@helpusdefend.com>
2025-02-05 15:42:46 -0600

    updated code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,5 @@
 import Header from "@/components/header";
-import Nosotros from "@/components/nosotros";
 import EmblaCarousel from "@/components/galeria";
-import slider from "@/components/highlight";
 import { EmblaOptionsType } from 'embla-carousel'
 import { config } from '@fortawesome/fontawesome-svg-core'
 import '@fortawesome/fontawesome-svg-core/styles.css'
@@ -9,8 +7,6 @@ config.autoAddCss = false
 import './embla.css'
 
 const OPTIONS: EmblaOptionsType = { dragFree: true, loop: true }
-const SLIDE_COUNT = 5
-const SLIDES = Array.from(Array(SLIDE_COUNT).keys())
 
 const IMAGES = [
   '/images/background.jpg',
