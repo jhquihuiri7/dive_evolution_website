cb72538759efa99f35bbf51e15c4d4221abc0251
Jhonatan Quihuiri <jumpinjhonatan@helpusdefend.com>
2025-02-05 22:24:42 -0600

    update images names

--- src/components/highlight.tsx
+++ src/components/highlight.tsx
@@ -6,10 +6,10 @@ import { EmblaOptionsType } from 'embla-carousel'
 const OPTIONS: EmblaOptionsType = { dragFree: true, loop: true }
 
 const IMAGES = [
-  '/images/car1.jpg',
-  '/images/car2.jpg',
-  '/images/car3.jpg',
-  '/images/car4.jpg'
+  '/images/car1.png',
+  '/images/car2.png',
+  '/images/car3.png',
+  '/images/car4.png'
 ];
 
 const Highlight:FunctionComponent = () => {
