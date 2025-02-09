1f1c8cb12f4167d468989ee136fbc48ac75ec545
Jhonatan Quihuiri <jumpinjhonatan@helpusdefend.com>
2025-02-09 12:15:50 -0600

    opinions responsive design updated

--- src/components/opinions.tsx
+++ src/components/opinions.tsx
@@ -18,14 +18,31 @@ const Opinions: FunctionComponent = () => {
 
     return () => clearInterval(interval);
   }, []);
-
+  const [isLandscape, setIsLandscape] = useState<boolean | null>(null);
+  const [isMobile, setIsMobile] = useState<boolean>(false); // Nuevo estado para identificar si es móvil
+      
+  useEffect(() => {
+      if (typeof window !== "undefined") {
+          const checkIfMobile = window.innerWidth <= 900; // Definir un límite para dispositivos móviles
+          setIsMobile(checkIfMobile); // Actualizar el estado según el tamaño de la ventana
+          setIsLandscape(window.innerWidth > window.innerHeight);
+          
+          const handleResize = () => {
+              setIsMobile(window.innerWidth <= 900); // Verificar en cada redimensionado si es móvil
+              setIsLandscape(window.innerWidth > window.innerHeight);
+          };
+          window.addEventListener("resize", handleResize);
+          return () => window.removeEventListener("resize", handleResize);
+      }
+  }, []);
+  if (isLandscape === null) return null;
   return (
     <div className='h-screen w-full bg-[url("https://res.cloudinary.com/logicielapplab/image/upload/v1739079631/DIVE_EVOLUTION_2025/REVIEWS/reviews_background_ozbzkr.webp")] bg-cover bg-center flex flex-col justify-center items-center'>
-      <div className='bg-white w-[50%] h-[60%] py-10 px-20 flex flex-col justify-around items-center'>
-        <h3 className='text-[40px] leading-[45px] font-black'>¿QUE DICEN NUESTROS CLIENTES DE NOSOTROS?</h3>
+      <div className={`px-20 ${(isMobile && isLandscape) ? "w-[80%] h-[70%] mt-10": (isMobile) ? "w-[70%] h-[70%] px-5" : "w-[50%] h-[60%]"} bg-white py-10 flex flex-col justify-around items-center`}>
+        <h3 className={`${(isMobile && isLandscape) ? "text-[30px] leading-[35px]" : (isMobile) ? "text-[30px] leading-[35px]" : "text-[40px] leading-[45px]"} font-black`}>¿QUE DICEN NUESTROS CLIENTES DE NOSOTROS?</h3>
         <div className='w-full flex flex-row justify-between'>
             <span className='font-bold text-6xl text-black'>&ldquo;</span>
-            <p className='italic text-gray-600 text-center px-10 pt-10'>{reviews[index].review}</p>
+            <p className='italic text-gray-600 text-center px-[5%] pt-10'>{reviews[index].review}</p>
             <span className='font-bold text-6xl text-black'>&rdquo;</span>
         </div>
         <p className='text-sm font-semibold'>{reviews[index].author}</p>
