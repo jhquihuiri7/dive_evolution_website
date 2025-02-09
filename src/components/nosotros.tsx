092b5213c20601a4e777e4f07d012787fc7d4b28
Jhonatan Quihuiri <jumpinjhonatan@helpusdefend.com>
2025-02-08 22:40:28 -0600

    nosotros responsive design

--- src/components/nosotros.tsx
+++ src/components/nosotros.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { FunctionComponent } from "react";
+import React, { FunctionComponent, useState, useEffect } from "react";
 
 const Nosotros: FunctionComponent = () => {
     
@@ -37,8 +37,30 @@ const Nosotros: FunctionComponent = () => {
 
     const marginTopValues: string[] = ["0%", "5%", "10%", "10%", "5%", "0%"];
 
+    const [isLandscape, setIsLandscape] = useState<boolean | null>(null);
+        const [isMobile, setIsMobile] = useState<boolean>(false); // Nuevo estado para identificar si es móvil
+            
+        useEffect(() => {
+            if (typeof window !== "undefined") {
+                const checkIfMobile = window.innerWidth <= 900; // Definir un límite para dispositivos móviles
+                setIsMobile(checkIfMobile); // Actualizar el estado según el tamaño de la ventana
+    
+                setIsLandscape(window.innerWidth > window.innerHeight);
+                
+                const handleResize = () => {
+                    setIsMobile(window.innerWidth <= 900); // Verificar en cada redimensionado si es móvil
+                    setIsLandscape(window.innerWidth > window.innerHeight);
+                };
+    
+                window.addEventListener("resize", handleResize);
+                return () => window.removeEventListener("resize", handleResize);
+            }
+        }, []);
+    
+        if (isLandscape === null) return null;
+
     return (
-        <div className="w-full h-screen flex flex-col justify-around items-center p-10">
+        <div className={`${(isMobile && isLandscape) ? "h-fit" : "h-screen" } w-full flex flex-col justify-around items-center p-10`}>
             {/* TÍTULO */}
             <div className='w-[300px] mt-6 flex flex-col justify-center items-center'>
                 <h2 className='text-6xl font-bold text-black'>Nosotros</h2>
@@ -46,14 +68,14 @@ const Nosotros: FunctionComponent = () => {
             </div>
 
             {/* IMÁGENES */}
-            <div className="w-[1000px] h-[60%] flex flex-row justify-around">
+            <div className={`flex flex-row justify-around ${(isMobile && isLandscape) ? "w-full h[200px]" : (isMobile) ? "w-full h-[60%] flex-wrap" : "w-[75%] h-[60%]"}`}>
                 {images.map((image, index) => (
                     <div
                     key={index}
-                    className="w-[300px] h-[80%] flex flex-col justify-end bg-cover bg-center transition-transform duration-300 hover:scale-150 group"
+                    className={`flex flex-col justify-end w-[15%] ${(isMobile && isLandscape) ? "h-full": (isMobile) ? "h-[30%] w-[45%]" :"h-[70%]"} bg-cover bg-center transition-transform duration-300 hover:scale-150 group`}
                     style={{
                         backgroundImage: `url('${image.img}')`,
-                        marginTop: marginTopValues[index],
+                        marginTop: (isMobile && isLandscape) ? "10px" : (isMobile) ? "10px" : marginTopValues[index],
                     }}
                 >
                     <div className="w-full h-[100px] bg-[#163E64] hidden group-hover:flex items-center justify-center text-white text-center px-2">
@@ -66,7 +88,7 @@ const Nosotros: FunctionComponent = () => {
 
             {/* TEXTO */}
             <span
-                className={`text-black text-lg text-center px-[25%] mt-5`}
+                className={` ${(isMobile && isLandscape) ? "px-[10%]" : (isMobile) ? "px-[5%]" :"px-[25%]"} text-black text-lg text-center mt-5`}
             >
                 {text}
             </span>
