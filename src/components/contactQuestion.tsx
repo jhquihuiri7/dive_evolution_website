5346be059c9c58fe153c5dd3c2e1d09462510da2
Jhonatan Quihuiri <jumpinjhonatan@helpusdefend.com>
2025-02-19 22:29:16 -0500

    contact responsive design

--- src/components/contactQuestion.tsx
+++ src/components/contactQuestion.tsx
@@ -5,8 +5,8 @@ const ContactQuestion:FunctionComponent = () => {
     const text = "¿Tienes dudas o necesitas ayuda con tu reserva? Nuestro equipo está listo para asistirte."
     const subtext = "¡Contáctanos y hagamos realidad tu próxima aventura!"
     return (
-        <div className='w-full my-20 px-[20%] text-center text-4xl flex flex-row justify-center'>
-          {text}<br/>{subtext}
+        <div className='w-full my-20 px-[20%] text-center text-4xl flex flex-col justify-center'>
+          {text}<br/><p className='hidden sm:flex'>{subtext}</p>
         </div>
     );
 }
